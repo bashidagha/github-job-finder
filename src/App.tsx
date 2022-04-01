a67b6d330b17ef710cabcab2a866560b35f7b7f8
Farshid Karimi <farshidk49@gmail.com>
2022-04-01 11:22:10 +0430

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import banner from '../assets/backgroundImg.png';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title mt-2"><span>Github</span> Jobs</h1>
+      <img src={banner}/>
+    </div>
+  );
+}
+
+export default App;
